4adbc1a8eb78f0a1dbed3668b55b261e26ffd976
rahats161 <rahat161@yahoo.com>
2023-05-28 18:46:52 -0500

    Use a while loop

--- chorus-while.js
+++ chorus-while.js
@@ -5,3 +5,9 @@ while (repeat < 10) {
   repeat++;
 }
 console.log("Until the sun comes up!");
+
+
+for (let repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
