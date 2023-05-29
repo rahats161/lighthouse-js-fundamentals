4659b5518bb47e814c2077a0912c43da7b87b473
rahats161 <rahat161@yahoo.com>
2023-05-28 22:00:16 -0500

    function

---
+++ loopy.js
@@ -0,0 +1,15 @@
+for (let i=100; i<=200; i++)
+{
+	if (i%3===0)
+	{
+           console.log("Loopy");
+	}
+	if (i%4===0)
+	{
+		console.log("LightHouse");
+	}
+	 if (i%3===0 && i%4===0)
+	{
+		console.log("Loopy Lighthouse");
+	}
+}
